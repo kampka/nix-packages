8122e784f276964e85cda5eddb9ff5ac078d546a
Christian Kampka <christian@kampka.net>
2018-08-05 14:49:29 +0200

    Add zsh-history package

--- default.nix
+++ default.nix
@@ -3,4 +3,6 @@
 
 {
   nixify = pkgs.callPackage ./pkgs/nixify {};
+
+  zsh-history = pkgs.callPackage ./pkgs/zsh-history {};
 }
---
+++ pkgs/zsh-history/deps.nix
@@ -0,0 +1,56 @@
+[
+  {
+    goPackagePath = "github.com/BurntSushi/toml";
+    fetch = {
+      type = "git";
+      url = "https://github.com/BurntSushi/toml";
+      rev = "a368813c5e648fee92e5f6c30e3944ff9d5e8895";
+      sha256 = "1sjxs2lwc8jpln80s4rlzp7nprbcljhy5mz4rf9995gq93wqnym5";
+    };
+  }
+  {
+    goPackagePath = "github.com/fatih/structs";
+    fetch = {
+      type = "git";
+      url = "https://github.com/fatih/structs";
+      rev = "ebf56d35bba727c68ac77f56f2fcf90b181851aa";
+      sha256 = "03yychj7aqavwz4djh3mxsxs73lgi32cxbabiy2bqj3kdq9bcfmc";
+    };
+  }
+  {
+    goPackagePath = "github.com/mattn/go-runewidth";
+    fetch = {
+      type = "git";
+      url = "https://github.com/mattn/go-runewidth";
+      rev = "ce7b0b5c7b45a81508558cd1dba6bb1e4ddb51bb";
+      sha256 = "0lc39b6xrxv7h3v3y1kgz49cgi5qxwlygs715aam6ba35m48yi7g";
+    };
+  }
+  {
+    goPackagePath = "github.com/mattn/go-sqlite3";
+    fetch = {
+      type = "git";
+      url = "https://github.com/mattn/go-sqlite3";
+      rev = "b3511bfdd742af558b54eb6160aca9446d762a19";
+      sha256 = "1v41is0h05p4jgv8nal3l8lcdcggylsyblq0ihycnprkdzwls808";
+    };
+  }
+  {
+    goPackagePath = "github.com/nsf/termbox-go";
+    fetch = {
+      type = "git";
+      url = "https://github.com/nsf/termbox-go";
+      rev = "5c94acc5e6eb520f1bcd183974e01171cc4c23b3";
+      sha256 = "1fi8imdgwvlsgifw2qfl3ww0lsrgkfsimkzz7bnrq41nar78s0fw";
+    };
+  }
+  {
+  goPackagePath = "gopkg.in/gorp.v1";
+  fetch = {
+    type = "git";
+    url = "https://gopkg.in/gorp.v1";
+    rev = "c87af80f3cc5036b55b83d77171e156791085e2e";
+    sha256 = "05pypdf0ngvhkdfnhn355pmyfm7pk0pxck9ym8qfab3qqkhqk0qr";
+  };
+}
+]
