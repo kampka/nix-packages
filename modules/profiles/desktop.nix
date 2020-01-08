42406182e0cc14a33861d0ad5f1861884ea8b175
Christian Kampka <christian@kampka.net>
2020-01-08 16:05:50 +0100

    [profiles/desktop] Add neovim overlay with native plugin configurations

--- modules/profiles/desktop.nix
+++ modules/profiles/desktop.nix
@@ -7,6 +7,23 @@ let
   cfg = config.kampka.profiles.desktop;
   common = import ./common.nix { inherit config pkgs lib; };
 
+  neovim-overlay = self: super: let
+    vimConfigure = {
+      customRC = ''
+        let s:localVimConfig = $HOME . "/.config/nvim/init.vim"
+        if filereadable(expand(s:localVimConfig))
+          exe 'source' s:localVimConfig
+        endif
+      '';
+      packages.kampka-defaults.opt = with pkgs.vimPlugins; [ LanguageClient-neovim ];
+    };
+  in
+    {
+      neovim = super.neovim.override {
+        configure = vimConfigure;
+        extraMakeWrapperArgs = "--set MYVIMRC ${pkgs.vimUtils.vimrcFile vimConfigure}";
+      };
+    };
 in
 {
 
@@ -16,6 +33,8 @@ in
 
   config = mkIf cfg.enable (
     recursiveUpdate common rec{
+
+      nixpkgs.overlays = [ neovim-overlay ];
 
       boot.loader.grub.splashImage = null;
 
@@ -68,8 +87,7 @@ in
       # Typically needed for wifi drivers and the like
       hardware.enableRedistributableFirmware = mkDefault true;
 
-      environment.systemPackages = common.environment.systemPackages ++ [ 
-      ] ++(
+      environment.systemPackages = common.environment.systemPackages ++ [] ++ (
         with pkgs; [
           ctags
           git
