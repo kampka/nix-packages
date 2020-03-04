e99d573bd1ddba2198a779be973cec1aae9f9efb
Christian Kampka <christian@kampka.net>
2020-03-04 14:50:33 +0100

    [modules/nginx] Pin default ssl ciphers on 20.03 for TLSv1.2 compatibility

--- modules/services/nginx/default.nix
+++ modules/services/nginx/default.nix
@@ -49,6 +49,12 @@ in
       error_log stderr debug;
     '';
 
+    # TODO-nixos-upgrade
+    # The current defaults for ssl ciphers in NixOS are not compatible with TLSv1.2
+    # See https://github.com/NixOS/nixpkgs/pull/80952
+    # Until this is resolved, use recommended ciphers from Mozillas 
+    # SSL configuration generator at https://ssl-config.mozilla.org/
+    services.nginx.sslCiphers = mkDefault "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384";
     services.nginx.sslDhparam = mkIf cfg.generateDhParams (
       mkDefault
         "${config.services.nginx.stateDir}/dhparams-${toString cfg.dhParamBytes}.pem"
